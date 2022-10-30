9e86daa0c8a152a0bc364e699cd404af8a52d30a
aenesgur <aenesgur@gmail.com>
2022-10-30 22:47:04 +0300

    Update path for product service

--- product-service/src/main/java/com/aenesgur/productservice/controller/HelloController.java
+++ product-service/src/main/java/com/aenesgur/productservice/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/api/products/hello")
 public class HelloController {
 
     @GetMapping
